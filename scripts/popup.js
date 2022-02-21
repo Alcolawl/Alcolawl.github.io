bd426ab02945e5f6845f9411f736832716fcb97f
Alcolawl <tdean@live.esu.edu>
2022-02-21 00:31:04 -0500

    Remove silly legal popup

--- scripts/popup.js
+++
@@ -1,6 +0,0 @@
-﻿$(document).ready(function () {
-    if ($.cookie('popup') != 1) {
-        $.cookie('popup', '1', { expires : 10 });
-        document.getElementById('popup').style.display = 'block'; document.getElementById('fade').style.display = 'block';
-    }
-});
