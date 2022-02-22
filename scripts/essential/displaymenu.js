6abadbdedeb1231e19eeef131d831c0f24b7ebe3
Alcolawl <tdean@live.esu.edu>
2022-02-22 14:02:17 -0500

    Introduce HTML and Images for new Hambuger Menu

--- scripts/essential/displaymenu.js
+++ scripts/essential/displaymenu.js
@@ -1,18 +1,17 @@
 ﻿$(document).ready(function () {
     var click = 0;
-    var div = document.getElementById('hamburger');
     var menu = document.getElementById('navMenu');
 
     $("#hamburger").click(function () {
         if (click == 1)                         //display menu on first click
         {
-            div.style.backgroundColor = '#222222';
+            /*document.getElementById('hamburger').src="/images/hamburger.gif";*/
             menu.style.visibility = 'hidden';
             click = 0;
         }
         else                                    //hide menu on second click
         {
-            div.style.backgroundColor = '#00C9FF';
+            /*document.getElementById('hamburger').src="/images/hamburger_active.gif";*/
             menu.style.visibility = 'visible';
             click = 1;
         }     
