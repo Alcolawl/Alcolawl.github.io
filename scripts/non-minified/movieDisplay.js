f0cbb85d482b8963a0530b790da0079a7b3ad2b8
Alcolawl <tdean@live.esu.edu>
2023-06-22 10:04:13 -0400

    Switch to using jquery.csv plugin to display movie lists
    This greatly improves maintainability of the movie list, while also allowing heavy optimization of several scripts used to display it to the web page. End result is less work for me and less work for the server and your device

--- scripts/non-minified/movieDisplay.js
+++ scripts/non-minified/movieDisplay.js
@@ -1,32 +1,25 @@
-﻿$(document).ready(function () {
-    $.ajax({
-        type: "GET",
-        url: "../xml/movielist.xml",
-        dataType: "xml",
-        success: function (xml) {
-            var x = 1;
-            var cell = "<td>";
-            var cellEnd = "</td>";
-            var link = '<a href="';
-            var linkMid = '">';
-            var linkEnd = "</a>";
-            var row = '<tr>';
-            var rowEnd = '</tr>';
+﻿$.get( "../xml/movielist.csv", function(input){
+    var movielist = $.csv.toArrays(input);
+    var link = '<a href="';
+    var linkMid = '">';
+    var linkEnd = "</a>";
+    var output = "";
 
-            $(xml).find('movie').each(function () {                                           //For each <Movie> tag, retrieve the following:
-                var scale = $(this).find('drunkscale').text()                                 //Retreive Movie's Drunk Scale
-                scale = cell + scale + cellEnd;                                               //<td>scale</td>
-                var title = $(this).find('title').text()                                      //Retreive Movie's Title
-                var url = $(this).find('link').text()                                         //Retreive Movie's URL
-                title = cell + link + url + linkMid + title + linkEnd + cellEnd;              //<td><a href="url">title</a></td>
-                var runtime = $(this).find('runtime').text()                                  //Retrieve Movie's Run Time
-                runtime = cell + runtime + cellEnd;                                           //<td>runtime</td>
-                var author = $(this).find('author').text()                                    //Retrieve Movie's Blurb
-                author = cell + author + cellEnd;                                             //<td>author</td>
-                var output = row + scale + title + runtime + author + rowEnd;                 //Create Output Block
-                $("#fullList").append(output);                                                //Output to table
-                x++;
-            })
-        }
-    })
-});
+    for (var count = 1; count<movielist.length; count++)                        // Iterate through each movie / line of csv
+    {
+        var title = (movielist[count][0])                                       // Retreive Movie's Title                
+        var scale = (movielist[count][2])                                       // Retreive Movie's Drunk Scale
+        var url = (movielist[count][1])                                         // Retreive Movie's URL
+        var runtime = (movielist[count][3])                                     // Retrieve Movie's Run Time
+        var author = (movielist[count][4])                                      // Retrieve Movie's Blurb
+        scale = "<td>" + scale + "</td>";                                       // <td>scale</td>
+        title = "<td>" + link + url + linkMid + title + linkEnd + "</td>";      // <td><a href="url">title</a></td>
+        runtime = "<td>" + runtime + "</td>";                                   // <td>runtime</td>
+        author = "<td>" + author + "</td>";                                     // <td>author</td>
+
+        var rowOut = scale + title + runtime + author;                          // Create and populate new row
+        output = output + "<tr>" + rowOut + "</tr>";                            // Add new row to final output
+
+    }
+    document.getElementById("fullList").innerHTML = output;                     // Output HTML results to web page
+})
--- scripts/non-minified/movieGrab.js
+++ scripts/non-minified/movieGrab.js
@@ -1,165 +1,63 @@
-﻿function go(choice) {
+﻿function go(choice){
 
-    var nextChar = choice;
-    nextChar++;
+    $.get( "../xml/movielist.csv", function(input){
+        var movielist = $.csv.toArrays(input);
+        var link = '<a href="';
+        var linkMid = '">';
+        var linkEnd = "</a>";
+        var output = "";
+        var header = '<tr><th class="center" width="25%">Drunk Scale (1-7)</th><th class="center" width="50%">Title</th><th class="center" width="10%">Run Time</th><th class="center">Author</th></tr>';
 
-    $.ajax({
-        type: "GET",
-        url: "../xml/movielist.xml",
-        dataType: "xml",
-        success: function (xml) {
-            var link = '<a href="';
-            var linkMid = '">';
-            var linkEnd = "</a>";
-            var output = "";
-            var finalize = '<tr><th class="center" width="25%">Drunk Scale (1-7)</th><th class="center" width="50%">Title</th><th class="center" width="10%">Run Time</th><th class="center">Author</th></tr>';
+        for (var count = 1; count<movielist.length; count++)                            // Iterate through each movie / line of csv
+        {
+            var title = (movielist[count][0])                                           // Retreive Movie's Title
 
-            $(xml).find('movie').each(function ()                                                 //For each <Movie> tag, retrieve the following:
+            // If the chosen letter (choice) is a T, we must do some extra checking
+            if (choice == 84)
             {
-                var title = $(this).find('title').text()                                          //Retreive Movie's Title
-
-                if ((choice == title.substring(0, 1).charCodeAt()) || (title.substring(0, 3) == "A " + String.fromCharCode(choice)))     //If the first letter of the movie starts with the user's chosen letter OR If the movie starts with A_ and the user's chosen letter
-                {
-                    var scale = $(this).find('drunkscale').text()                                 //Retreive Movie's Drunk Scale
-                    scale = "<td>" + scale + "</td>";                                             //<td>scale</td>
-                    var url = $(this).find('link').text()                                         //Retreive Movie's URL
-                    title = "<td>" + link + url + linkMid + title + linkEnd + "</td>";            //<td><a href="url">title</a></td>
-                    var runtime = $(this).find('runtime').text()                                  //Retrieve Movie's Run Time
-                    runtime = "<td>" + runtime + "</td>";                                         //<td>runtime</td>
-                    var author = $(this).find('author').text()                                    //Retrieve Movie's Blurb
-                    author = "<td>" + author + "</td>";                                           //<td>author</td>
-                    var rowOut = scale + title + runtime + author;
-                    output = output + "<tr>" + rowOut + "</td>";
-                }
-
-                if ((title.substring(0, 5) == "The " + String.fromCharCode(choice)))              //If the movie starts with The_ and the user's chosen letter
-                {
-                    var scale = $(this).find('drunkscale').text()
-                    scale = "<td>" + scale + "</td>";
-                    var url = $(this).find('link').text()
-                    title = "<td>" + link + url + linkMid + title + linkEnd + "</td>";
-                    var runtime = $(this).find('runtime').text()
-                    runtime = "<td>" + runtime + "</td>";
-                    var author = $(this).find('author').text()
-                    author = "<td>" + author + "</td>";
-                    var rowOut = scale + title + runtime + author;
-                    output = output + "<tr>" + rowOut + "</td>";
-                }
-
-                if (choice == 90) { document.getElementById("results").innerHTML = finalize + output; }     //If the user chose the letter "Z", use this condition to break the loop instead. Special Case!
-                if (choice == 83)                                                                           //If the user chose the letter "S", use this condition to break the loop instead. Special Case!
-                {
-                    if ((title.substring(0, 5) == "The T")) {
-                        document.getElementById("results").innerHTML = finalize + output;
-                        return false;
+                // If the movie title begins with a "T", proceed
+                if (choice == title.substring(0, 1).charCodeAt()){
+                    // Does the movie begin with the word "The"?
+                    if (title.substring(0, 3) == "The"){
+                        // If the movie starts with "The" but the second word does not start with a "T", iterate the loop
+                        if (title.substring(0, 5) != "The " + String.fromCharCode(choice)){continue;}
                     }
                 }
-                else if ((String.fromCharCode(nextChar) == title.substring(0, 1)) && (title.substring(0, 5) != "The " + String.fromCharCode(choice))) //Otherwise exit normally
-                {
-                    document.getElementById("results").innerHTML = finalize + output;
-                    return false;
-                }
-            })
-        }
-    })
-};
+            }
 
-function tCase(choice) {
-
-    var nextChar = choice;
-    nextChar++;
-
-    $.ajax({
-        type: "GET",
-        url: "../xml/movielist.xml",
-        dataType: "xml",
-        success: function (xml) {
-            var link = '<a href="';
-            var linkMid = '">';
-            var linkEnd = "</a>";
-            var output = "";
-            var finalize = '<tr><th class="center" width="25%">Drunk Scale (1-7)</th><th class="center" width="50%">Title</th><th class="center" width="10%">Run Time</th><th class="center">Author</th></tr>';
-
-            $(xml).find('movie').each(function ()                                                 //For each <Movie> tag, retrieve the following:
+            // If the chosen letter (choice) matches the title (movielist[count][0]) or begins with an A or THE + the chosen letter
+            if ((choice == title.substring(0, 1).charCodeAt()) || (title.substring(0, 3) == "A " + String.fromCharCode(choice)) || (title.substring(0, 5) == "The " + String.fromCharCode(choice)))
             {
-                var title = $(this).find('title').text()                                          //Retreive Movie's Title
+                
+                var scale = (movielist[count][2])                                       // Retreive Movie's Drunk Scale
+                var url = (movielist[count][1])                                         // Retreive Movie's URL
+                var runtime = (movielist[count][3])                                     // Retrieve Movie's Run Time
+                var author = (movielist[count][4])                                      // Retrieve Movie's Blurb
+                scale = "<td>" + scale + "</td>";                                       // <td>scale</td>
+                title = "<td>" + link + url + linkMid + title + linkEnd + "</td>";      // <td><a href="url">title</a></td>
+                runtime = "<td>" + runtime + "</td>";                                   // <td>runtime</td>
+                author = "<td>" + author + "</td>";                                     // <td>author</td>
 
-                if ((title.substring(0, 3) == "A " + String.fromCharCode(choice)) || (title.substring(0, 5) == "The " + String.fromCharCode(choice)))          //If the movie starts with A_ OR The_ and the user's chosen letter 
-                {
-                    var scale = $(this).find('drunkscale').text()                                 //Retreive Movie's Drunk Scale
-                    scale = "<td>" + scale + "</td>";                                             //<td>scale</td>
-                    var url = $(this).find('link').text()                                         //Retreive Movie's URL
-                    title = "<td>" + link + url + linkMid + title + linkEnd + "</td>";            //<td><a href="url">title</a></td>
-                    var runtime = $(this).find('runtime').text()                                  //Retrieve Movie's Run Time
-                    runtime = "<td>" + runtime + "</td>";                                         //<td>runtime</td>
-                    var author = $(this).find('author').text()                                    //Retrieve Movie's Blurb
-                    author = "<td>" + author + "</td>";                                           //<td>author</td>
-                    var rowOut = scale + title + runtime + author;
-                    output = output + "<tr>" + rowOut + "</td>";
-                }
+                var rowOut = scale + title + runtime + author;                          // Create and populate new row
+                output = output + "<tr>" + rowOut + "</tr>";                            // Add new row to final output
+            }
 
-                if ((choice == title.substring(0, 1).charCodeAt()) && (title.substring(0, 4) != "The "))          //If the movie starts with A_ OR The_ and the user's chosen letter 
-                {
-                    var scale = $(this).find('drunkscale').text()                                 //Retreive Movie's Drunk Scale
-                    scale = "<td>" + scale + "</td>";                                             //<td>scale</td>
-                    var url = $(this).find('link').text()                                         //Retreive Movie's URL
-                    title = "<td>" + link + url + linkMid + title + linkEnd + "</td>";            //<td><a href="url">title</a></td>
-                    var runtime = $(this).find('runtime').text()                                  //Retrieve Movie's Run Time
-                    runtime = "<td>" + runtime + "</td>";                                         //<td>runtime</td>
-                    var author = $(this).find('author').text()                                    //Retrieve Movie's Blurb
-                    author = "<td>" + author + "</td>";                                           //<td>author</td>
-                    var rowOut = scale + title + runtime + author;
-                    output = output + "<tr>" + rowOut + "</td>";
-                }
-
-
-                if ((String.fromCharCode(nextChar) == title.substring(0, 1)) && (title.substring(0, 5) != "The " + String.fromCharCode(choice))) {
-                    document.getElementById("results").innerHTML = finalize + output;
-                    return false;
-                }
-            })
-        }
-    })
-};
-
-function numCase() {
-
-    var nextChar = "A";
-
-    $.ajax({
-        type: "GET",
-        url: "../xml/movielist.xml",
-        dataType: "xml",
-        success: function (xml) {
-            var link = '<a href="';
-            var linkMid = '">';
-            var linkEnd = "</a>";
-            var output = "";
-            var finalize = '<tr><th class="center" width="25%">Drunk Scale (1-7)</th><th class="center" width="50%">Title</th><th class="center" width="10%">Run Time</th><th class="center">Author</th></tr>';
-
-            $(xml).find('movie').each(function ()                                                 //For each <Movie> tag, retrieve the following:
+            if (choice == 35 && (title.substring(0, 1).charCodeAt()) >= 48 && (title.substring(0, 1).charCodeAt()) <= 57)     //If the movie starts with a number
             {
-                var title = $(this).find('title').text()                                          //Retreive Movie's Title
+                
+                var scale = (movielist[count][2])                                       // Retreive Movie's Drunk Scale
+                var url = (movielist[count][1])                                         // Retreive Movie's URL
+                var runtime = (movielist[count][3])                                     // Retrieve Movie's Run Time
+                var author = (movielist[count][4])                                      // Retrieve Movie's Blurb
+                scale = "<td>" + scale + "</td>";                                       // <td>scale</td>
+                title = "<td>" + link + url + linkMid + title + linkEnd + "</td>";      // <td><a href="url">title</a></td>
+                runtime = "<td>" + runtime + "</td>";                                   // <td>runtime</td>
+                author = "<td>" + author + "</td>";                                     // <td>author</td>
 
-                if (title.substring(0, 1).charCodeAt() >= 48 && title.substring(0, 1).charCodeAt() <= 57)     //If the movie starts with a number
-                {
-                    var scale = $(this).find('drunkscale').text()                                 //Retreive Movie's Drunk Scale
-                    scale = "<td>" + scale + "</td>";                                             //<td>scale</td>
-                    var url = $(this).find('link').text()                                         //Retreive Movie's URL
-                    title = "<td>" + link + url + linkMid + title + linkEnd + "</td>";            //<td><a href="url">title</a></td>
-                    var runtime = $(this).find('runtime').text()                                  //Retrieve Movie's Run Time
-                    runtime = "<td>" + runtime + "</td>";                                         //<td>runtime</td>
-                    var author = $(this).find('author').text()                                    //Retrieve Movie's Blurb
-                    author = "<td>" + author + "</td>";                                           //<td>author</td>
-                    var rowOut = scale + title + runtime + author;
-                    output = output + "<tr>" + rowOut + "</td>";
-                }
-
-                if (nextChar == title.substring(0, 1)) {                                          //Exit when the next movie begins with the letter A
-                    document.getElementById("results").innerHTML = finalize + output;
-                    return false;
-                }
-            })
+                var rowOut = scale + title + runtime + author;                          // Create and populate new row
+                output = output + "<tr>" + rowOut + "</tr>";                            // Add new row to final output
+            }
         }
+        document.getElementById("results").innerHTML = header + output;                 // Output HTML results to web page
     })
-};
+}
