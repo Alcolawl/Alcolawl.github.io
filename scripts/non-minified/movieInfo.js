f638bd149cc87304f9decf1929486042a08073a5
Alcolawl <tdean@live.esu.edu>
2023-06-29 14:58:39 -0400

    Update MovieInfo Script to use CSV instead of XML

--- scripts/non-minified/movieInfo.js
+++ scripts/non-minified/movieInfo.js
@@ -1,47 +1,57 @@
 ﻿$(window).on('load', function() {
-    $.ajax({
-        type: "GET",
-        url: "../../xml/movielist.xml",
-        dataType: "xml",
-        success: function (xml) {
-            var img = '<img src="';
-            var imgEnd = '"/>';
+    $.get( "../../xml/movielist.csv", function(input){
+        var movielist = $.csv.toArrays(input);
+        var img = '<img src="';
+        var imgEnd = '"/>';
 
-            $(xml).find('movie').each(function () {
-                var title = $(this).find('title').text()                                      //Retreive Movie's Title
-                if (title == document.title) {                                                //If title = the page's Title
-                    var scale = $(this).find('drunkscale').text()                             //Retreive Movie's Drunk Scale
-                    var runtime = $(this).find('runtime').text()                              //Retrieve Movie's Run Time
+        for (var count = 1; count<movielist.length; count++)                                // Iterate through each movie / line of csv
+        {
+            var title = (movielist[count][0])                                               // Retreive Movie's Title
 
+            if (title == document.title) {                                                  // If title = the page's Title
+                var scale = (movielist[count][2])                                           // Retreive Movie's Drunk Scale
+                var runtime = (movielist[count][3])                                         // Retrieve Movie's Run Time
 
-                    if (scale == "3")                                                            //If statements to create img for drunkscale
-                        scale = img + "../../images/drunkscale/three.png" + imgEnd;
-                    if (scale == "3.5")
-                        scale = img + "../../images/drunkscale/threehalf.png" + imgEnd;
-                    if (scale == "4")
-                        scale = img + "../../images/drunkscale/four.png" + imgEnd;
-                    if (scale == "4.5")
-                        scale = img + "../../images/drunkscale/fourhalf.png" + imgEnd;
-                    if (scale == "5")
-                        scale = img + "../../images/drunkscale/five.png" + imgEnd;
-                    if (scale == "5.5")
-                        scale = img + "../../images/drunkscale/fivehalf.png" + imgEnd;
-                    if (scale == "6")
-                        scale = img + "../../images/drunkscale/six.png" + imgEnd;
-                    if (scale == "6.5")
-                        scale = img + "../../images/drunkscale/sixhalf.png" + imgEnd;
-                    if (scale == "7")
-                        scale = img + "../../images/drunkscale/seven.png" + imgEnd;
-                    if (scale == "8")
-                        scale = img + "../../images/drunkscale/eight.png" + imgEnd;
-
-
-                    $("#drunkscale").html(scale);                                                 //Output Drunk Scale to Page
-                    runtime = "Run Time: " + runtime;
-                    $("#runtime").html(runtime);                                                  //Output Runtime to Page
+                switch(scale){
+                    case '3':
+                        scale = "../../images/drunkscale/three.png";
+                        break;
+                    case '3.5':
+                        scale = "../../images/drunkscale/threehalf.png";
+                        break;
+                    case '4':
+                        scale = "../../images/drunkscale/four.png";
+                        break;
+                    case '4.5':
+                        scale = "../../images/drunkscale/fourhalf.png";
+                        break;
+                    case '5':
+                        scale = "../../images/drunkscale/five.png";
+                        break;
+                    case '5.5':
+                        scale = "../../images/drunkscale/fivehalf.png";
+                        break;
+                    case '6':
+                        scale = "../../images/drunkscale/six.png";
+                        break;
+                    case '6.5':
+                        scale = "../../images/drunkscale/sixhalf.png";
+                        break;
+                    case '7':
+                        scale = "../../images/drunkscale/seven.png";
+                        break;
+                    case '8':
+                        scale = "../../images/drunkscale/eight.png";
+                        break;
+                    default:
+                        break;
                 }
-            })
+
+                scale = img + scale + imgEnd;                                                // Set HTML for DrunkScale image
+                $("#drunkscale").html(scale);                                                // Output Drunk Scale to Page
+                runtime = "Run Time: " + runtime;
+                $("#runtime").html(runtime);                                                 // Output Runtime to Page
+            }
         }
     })
-});
-
+})
