bcd2fd7a7a74e249dafecdb4f9001bc283df54e5
Alcolawl <tdean@live.esu.edu>
2024-03-28 17:50:06 -0400

    Documentation update for movieDisplay.js

--- scripts/non-minified/movieDisplay.js
+++ scripts/non-minified/movieDisplay.js
@@ -1,4 +1,6 @@
-﻿$.get( "../xml/movielist.csv", function(input){
+﻿// Retirieves and displays the full list of movies
+
+$.get( "../xml/movielist.csv", function(input){
     var movielist = $.csv.toArrays(input);
     var link = '<a href="';
     var linkMid = '">';
@@ -14,9 +16,9 @@
         var author = (movielist[count][4])                                      // Retrieve Movie's Blurb
         scale = "<td>" + scale + "</td>";                                       // <td>scale</td>
         title = "<td>" + link + url + linkMid + title + linkEnd + "</td>";      // <td><a href="url">title</a></td>
-        runtime = "<td class=\"runtime\">" + runtime + "</td>";                                   // <td>runtime</td>
+        runtime = "<td class=\"runtime\">" + runtime + "</td>";                 // <td>runtime</td>
 
-        var rowOut = scale + title + runtime;                          // Create and populate new row
+        var rowOut = scale + title + runtime;                                   // Create and populate new row
         output = output + "<tr>" + rowOut + "</tr>";                            // Add new row to final output
 
     }
