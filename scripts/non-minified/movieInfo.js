774df5bb571114912f18aa90724d209b528964cb
Alcolawl <tdean@live.esu.edu>
2024-03-28 17:52:48 -0400

    Update movieInfo.js to use indexOf function instead of for loop
    Using IndexOf should increase performance as we're no longer iterating through the entire CSV until the movie title is found. The previous approach would result in worse performance the further in the alphabet you progressed

--- scripts/non-minified/movieInfo.js
+++ scripts/non-minified/movieInfo.js
@@ -1,57 +1,54 @@
-﻿$(window).on('load', function() {
+﻿// Retrieves information for the specific user-selected movie
+
+$(window).on('load', function() {
     $.get( "../../xml/movielist.csv", function(input){
         var movielist = $.csv.toArrays(input);
         var img = '<img src="';
         var imgEnd = '"/>';
 
-        for (var count = 1; count<movielist.length; count++)                                // Iterate through each movie / line of csv
-        {
-            var title = (movielist[count][0])                                               // Retreive Movie's Title
+        const index = movielist.findIndex(inner => inner.indexOf(document.title) >= 0); // Find the index of the Movie within the 2D array movielist
 
-            if (title == document.title) {                                                  // If title = the page's Title
-                var scale = (movielist[count][2])                                           // Retreive Movie's Drunk Scale
-                var runtime = (movielist[count][3])                                         // Retrieve Movie's Run Time
+        var scale = (movielist[index][2])                                           // Retreive Movie's Drunk Scale
+        var runtime = (movielist[index][3])                                         // Retrieve Movie's Run Time
 
-                switch(scale){
-                    case '3':
-                        scale = "../../images/drunkscale/three.png";
-                        break;
-                    case '3.5':
-                        scale = "../../images/drunkscale/threehalf.png";
-                        break;
-                    case '4':
-                        scale = "../../images/drunkscale/four.png";
-                        break;
-                    case '4.5':
-                        scale = "../../images/drunkscale/fourhalf.png";
-                        break;
-                    case '5':
-                        scale = "../../images/drunkscale/five.png";
-                        break;
-                    case '5.5':
-                        scale = "../../images/drunkscale/fivehalf.png";
-                        break;
-                    case '6':
-                        scale = "../../images/drunkscale/six.png";
-                        break;
-                    case '6.5':
-                        scale = "../../images/drunkscale/sixhalf.png";
-                        break;
-                    case '7':
-                        scale = "../../images/drunkscale/seven.png";
-                        break;
-                    case '8':
-                        scale = "../../images/drunkscale/eight.png";
-                        break;
-                    default:
-                        break;
-                }
-
-                scale = img + scale + imgEnd;                                                // Set HTML for DrunkScale image
-                $("#drunkscale").html(scale);                                                // Output Drunk Scale to Page
-                runtime = "Run Time: " + runtime;
-                $("#runtime").html(runtime);                                                 // Output Runtime to Page
-            }
+        switch(scale){
+            case '3':
+                scale = "../../images/drunkscale/three.png";
+                break;
+            case '3.5':
+                scale = "../../images/drunkscale/threehalf.png";
+                break;
+            case '4':
+                scale = "../../images/drunkscale/four.png";
+                break;
+            case '4.5':
+                scale = "../../images/drunkscale/fourhalf.png";
+                break;
+            case '5':
+                scale = "../../images/drunkscale/five.png";
+                break;
+            case '5.5':
+                scale = "../../images/drunkscale/fivehalf.png";
+                break;
+            case '6':
+                scale = "../../images/drunkscale/six.png";
+                break;
+            case '6.5':
+                scale = "../../images/drunkscale/sixhalf.png";
+                break;
+            case '7':
+                scale = "../../images/drunkscale/seven.png";
+                break;
+            case '8':
+                scale = "../../images/drunkscale/eight.png";
+                break;
+            default:
+                break;
         }
+
+        scale = img + scale + imgEnd;                                                // Set HTML for DrunkScale image
+        $("#drunkscale").html(scale);                                                // Output Drunk Scale to Page
+        runtime = "Run Time: " + runtime;
+        $("#runtime").html(runtime);                                                 // Output Runtime to Page
     })
 })
